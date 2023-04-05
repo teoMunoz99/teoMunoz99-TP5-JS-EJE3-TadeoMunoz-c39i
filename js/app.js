119033ff827fbf3072680a73c1c1ab01689cf897
Tadeo Munoz <tadeomunoz09@hotmail.com>
2023-04-04 21:27:50 -0300

    agregue la funcion para crear una lista con la tarea ingresada

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
-//==========Declaro variables===========//
-let i = 0; //uso esta variable como contador
-let arregloTareas = [];//aqui guardo las tareas ingresadas
+//Declaro variables y traigo elementos
+const tareaPendiente = document.querySelector('input'); //input
+const botonAgregar = document.getElementById('botonAgregar');//boton "agregar"
+const ul = document.querySelector('ul'); //ul donde voy a agregar o borrar las tareas pendientes
 
-//===============funciones=============//
+//funciones
+function agregarTarea(e){
+    e.preventDefault();
+    
+    //guardo en la variable tarea el valor del input
+    let tarea = tareaPendiente.value;
 
-//funcion para agregar tareas en el arreglo
-function agregarTarea(){
-    arregloTareas[i] = document.getElementById('inputTareas').value;
-    i++;
+    //creo el li que voy a meter en el ul
+    const li = document.createElement('li');
+
+    //en la variable p guardo el texto que va tener el li
+    const p = document.createElement('p');
+
+    //guardo el contenido del input que estaba en la variable tarea dentro del p
+    p.textContent = tarea;
+
+    //añado el p en el li
+    li.appendChild(p);
+
+    //ahora añado el li al ul
+    ul.appendChild(li);
+    //borro el valor de input luego de agregar una tarea
+    tareaPendiente.value = '';
 }
+
+//agrego eventos
+botonAgregar.addEventListener('click', agregarTarea);
